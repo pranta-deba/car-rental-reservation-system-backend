6d81bbb7700736b3733eac91ec4acafa8ba4a7ad
Pranto Deb <rajprit0m@outlook.com>
2025-02-23 23:35:33 +0600

    [Custom Send Response] : created custom send response

--- src/app/modules/users/user.validation.ts
+++ src/app/modules/users/user.validation.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 
 export const userValidationSchema = z.object({
-  name: z.string().min(2, 'Name must be at least 2 characters'),
-  email: z.string().email('Invalid email address'),
-  role: z.enum(['user', 'admin']),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-  phone: z.string().regex(/^\d{10,15}$/, 'Phone number must be 10-15 digits'),
-  address: z.string().min(5, 'Address must be at least 5 characters'),
-  isDeleted: z.boolean().optional(),
+  body: z.object({
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+    user: z.object({
+      name: z.string().min(2, 'Name must be at least 2 characters'),
+      email: z.string().email('Invalid email address'),
+      role: z.enum(['user', 'admin']),
+      phone: z
+        .string()
+        .regex(/^\d{10,15}$/, 'Phone number must be 10-15 digits'),
+      address: z.string().min(5, 'Address must be at least 5 characters'),
+      isDeleted: z.boolean().optional(),
+    }),
+  }),
 });
