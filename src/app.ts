d3af2dc4a8c3cab72b156320060e53774b20d889
Pranto Deb <rajprit0m@outlook.com>
2025-02-23 22:10:51 +0600

    [User] : create user interface

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
+import status from 'http-status';
 
 const app: Application = express();
 
@@ -13,6 +14,7 @@ app.use(cors({ origin: ['http://localhost:5173'] }));
 // application route
 app.use('/api/v1', router);
 
+// root api
 app.get('/', async (req: Request, res: Response) => {
   res.send(`
     <!DOCTYPE html>
@@ -29,6 +31,15 @@ app.get('/', async (req: Request, res: Response) => {
     </body>
     </html>
   `);
+});
+
+// api not found
+app.use((req: Request, res: Response) => {
+  res.status(status.NOT_FOUND).json({
+    success: false,
+    message: 'API Not Found!',
+    error: '',
+  });
 });
 
 export default app;
