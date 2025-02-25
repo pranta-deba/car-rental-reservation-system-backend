87b874090cf445edb1361af2e60e616d64978a56
Pranto Deb <rajprit0m@outlook.com>
2025-02-25 21:08:09 +0600

    [Booking] : model

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,14 @@
+import { Schema } from 'mongoose';
+import { TBooking } from './booking.interface';
+
+const BookingSchema = new Schema<TBooking>(
+  {
+    date: { type: Date, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String },
+    totalCost: { type: Number, required: true, default: 0 },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    car: { type: Schema.Types.ObjectId, ref: 'Car', required: true },
+  },
+  { timestamps: true },
+);
